9cc0975382b18e773e3c1dc9eb8a876390180b2b
Inon Bercu <inonbr@gmail.com>
2021-07-27 12:45:45 +0300

    connected to mongoDb + created user model

--- back_end/server.js
+++ back_end/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const connectDB = require('./db/db');
+const User = require('./models/Users');
+
 const port = 5000;
 
 app.use(cors());
@@ -11,6 +14,23 @@ app.get('/api', (req, res) => {
   res.send('hello world');
 });
 
-app.listen(port, () => {
-  console.log(`🔵 app listening at http://localhost:${port}`);
+app.post('/api/createUser', (req, res) => {
+  const newUser = User({
+    userName: 'admin',
+    password: 'admin',
+  });
+
+  newUser.save().then((user) => {
+    return res.status(200).json({
+      msg: 'user saved successfully',
+      user: user,
+    });
+  });
+});
+
+connectDB().then(() => {
+  console.log('🔵 MongoDB connected...');
+  app.listen(port, () => {
+    console.log(`🟢 App listening at http://localhost:${port}`);
+  });
 });
